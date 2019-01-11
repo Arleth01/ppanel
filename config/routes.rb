364a2bd39614e76892756097bc302a8910dd6332
Arleth <Aontiveros@nyc.yearup.org>
2019-01-11 12:10:54 -0500

    nothing

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
 
   resources :homes
     
-  get '/
-#courses
-  
+  # get '/'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
+
